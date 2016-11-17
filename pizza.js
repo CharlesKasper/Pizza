417e2261ce4bd11e54b647dc4c74c2ec79c168cc
CharlesKasper <7ckasper@stpatrick.org>
2016-11-17 13:56:12 -0600

    Add files via upload

---
+++ pizza.js
@@ -0,0 +1,26 @@
+class pizza{
+  //type constructor below
+  constructor(s,m,v){
+    this.size=s;
+    this.meatToppings=[];
+    this.veggieTopping=[];
+  }
+  //type instance functions below
+sizecost(){
+  if (this.size="S"){
+    pizzaPrice=7.99;
+  }
+  elseif(this.size="M"){
+    pizzaPrice=9.99;
+  }
+  elseif(this.size="L"){
+    pizzaPrice=12.99;
+  }
+  elseif()
+}
+
+
+  //type class functions below
+
+
+}
